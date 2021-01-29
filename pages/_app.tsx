f7db7076f9beab5af5c169998362d417ed116e9b
Wesley Tam <hcwtam@gmail.com>
2021-01-29 11:43:07 +0800

    move next/head to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import React from 'react';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Wesley Tam | Web Developer</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css"
+        />
+        <link rel="stylesheet" href="/assets/font/stylesheet.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Navbar from '../components/Navbar/Navbar';
 import Landing from '../components/Landing/Landing';
 import About from '../components/About/About';
@@ -8,16 +7,6 @@ import Contact from '../components/Contact/Contact';
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Wesley Tam | Web Developer</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css"
-        />
-        <link rel="stylesheet" href="/assets/font/stylesheet.css" />
-      </Head>
-
       <Navbar />
       <Landing />
       <About />
